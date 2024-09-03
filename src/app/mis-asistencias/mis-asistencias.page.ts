f397082e922581ec841a1bc0e38d9d776d8e1e63
RafaOteiza <ra.oteiza@gmail.com>
2024-09-03 17:16:24 -0400

    Cambie las asignaturas por las del duoc

--- src/app/mis-asistencias/mis-asistencias.page.ts
+++ src/app/mis-asistencias/mis-asistencias.page.ts
@@ -10,22 +10,28 @@ export class MisAsistenciasPage {
   // Acá puse un array con asistencias, asi que si quieren más se pueden crear aquí y se listarán en el html. 
   asistencias = [
     {
-      titulo: 'Asistencia 1',
+      titulo: 'ÉTICA PARA EL TRABAJO',
       fecha: '2024-09-03',
-      ubicacion: 'Sala A',
-      descripcion: 'Descripción de la Asistencia 1'
+      ubicacion: 'SALA SJ-706',
+      descripcion: 'Sección EAY4470'
     },
     {
-      titulo: 'Asistencia 2',
+      titulo: 'PROGRAMACIÓN DE APLICACIONES MÓVILES',
       fecha: '2024-09-05',
-      ubicacion: 'Sala B',
-      descripcion: 'Descripción de la Asistencia 2'
+      ubicacion: 'SALA SJ-L7',
+      descripcion: 'Sección PGY4121'
     },
     {
-      titulo: 'Asistencia 3',
+      titulo: 'ESTADÍSTICA DESCRIPTIVA',
       fecha: '2024-09-07',
-      ubicacion: 'Sala C',
-      descripcion: 'Descripción de la Asistencia 3'
+      ubicacion: 'SALA SJ-L4',
+      descripcion: 'Sección MAT4140'
+    },
+    {
+      titulo: 'ESTADÍSTICA DESCRIPTIVA',
+      fecha: '2024-09-09',
+      ubicacion: 'SALA SJ-L4',
+      descripcion: 'Sección MAT4140'
     }
   ];
 
