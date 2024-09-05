120540c7c164d529712c453e4c1e3533e6786d31
anpachecos <an.pachecos@duocuc.cl>
2024-09-05 12:29:15 -0400

    boton modo oscuroOOOO

--- src/app/ajustes/ajustes.page.ts
+++ src/app/ajustes/ajustes.page.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AjustesPage implements OnInit {
 
-  constructor() { }
+
+  paletteToggle = false;
+
+  constructor() {
+    this.initDarkMode();
+   }
 
   ngOnInit() {
+
   }
 
+
+  initDarkMode(){
+
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // Initialize the dark palette based on the initial
+    // value of the prefers-color-scheme media query
+    this.initializeDarkPalette(prefersDark.matches);
+
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDark.addEventListener('change', (mediaQuery) => this.initializeDarkPalette(mediaQuery.matches));
+
+  }
+
+
+  initializeDarkPalette(isDark: any) {
+    this.paletteToggle = isDark;
+    this.toggleDarkPalette(isDark);
+  }
+
+  // Listen for the toggle check/uncheck to toggle the dark palette
+  toggleChange(ev: any) {
+    this.toggleDarkPalette(ev.detail.checked);
+  }
+
+  // Add or remove the "ion-palette-dark" class on the html element
+  toggleDarkPalette(shouldAdd: boolean) {
+    document.documentElement.classList.toggle('ion-palette-dark', shouldAdd);
+  
+}
 }
